3ea7aab1b129a359f876f63b34eaac979e2b87ff
AnupSharma160 <anupsharma9058@gmail.com>
2025-02-07 07:15:32 +0000

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail= "anupsharma9058@gmail.com"
+var accountPassword = "123456"
+accountcity = "Jaipur"
+
+// accountId = 2 are not allowed 
+accountEmail="hvbj.com"
+accountPassword="121212"
+accountcity="bengluru"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+ "use strict" //treat all js code as  newer version
+ // alert(3+3) we are using nodejs,not browser. we can in broswer 
+ // the pop up show the result on the display is 6.
+ console.log(3+3);//code readability should be high
+ console.log("Anup")
+  
+ let name = "hitesh"//String 
+ let age = 18   // constant
+ let isLoggedIn = false   //  boolean
+     // Primitive Datatypes
+ //number => 2 to power 53
+ //bignit
+ //String =>""
+ //null => Standalone value 
+ // undefined => the value not assign 
+ // symbol =>unique 
+
+
+ // object 
+
+ console.log(typeof null);
