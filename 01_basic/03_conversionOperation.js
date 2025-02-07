7ab61e2c4d72bb9995dc1c7cb1212955b1f9545f
AnupSharma160 <anupsharma9058@gmail.com>
2025-02-07 08:47:45 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//let score = true  
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);// user find the vvalue type of score 
+// we can write a string. string cannot convert into number and gives NAN
+//console.log(valueInNumber);
+
+
+// "33"=> 33
+//"33abc"=>NaN
+//true = 1 and false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true , 0=> false 
+// ""=> false 
+// "anup"=> true
+
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
