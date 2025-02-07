d9366db5bce174a77338d8ee6942417ecc0078ff
AnupSharma160 <anupsharma9058@gmail.com>
2025-02-07 16:18:53 +0000

    comparison

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,7 +16,7 @@
 let isLoggedIn = 1
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=> true , 0=> false 
 // ""=> false 
@@ -24,5 +24,51 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+//**************operations **************/
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(value);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "hello"
+let str2 = " anup"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122 first come string all code behave string 
+// console.log(1+2+"2");//32
+
+// console.log(3+4*5%3); //not good code its a confusing code or value  
+// console.log(+true); // 1
+// console.log(+"");  // 0
+
+let num1,num2,num3 
+ num1= num2= num3=2+2
+
+ let gameCounter =100
+ ++gameCounter; //101
+ console.log(gameCounter);
+ 
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log(undefined <=0);// all are comparison can be avoided 
+//because all are disturb the code
+console.log(undefined>0);
+console.log(undefined<0);
+
+// === strictly check 
+
+console.log("2" === 2); // strictly check answer is false because 
+//one is string and another is number both are different to each other 
+
+
+
+
+
+
+
+
+
