5399f0b5769defb4d38bdee32eeab316adb94521
AnupSharma160 <anupsharma9058@gmail.com>
2025-02-08 09:30:57 +0000

    datatypesummary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,26 @@
+const score =100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const outsideTemp = null 
+let userEmail;  // defined output because we can only define not declare 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+ console.log(id === anotherId);
+ 
+
+ const heros = ["shaktiman","naagraj","doga"] // this is an example of array
+ {}// currly bracket show or represent object In the 
+ // currly bracket we can write something known as object 
+let myObj= {
+name:"anup",
+age:21,
+
+ }
+
+ const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myfunction);
