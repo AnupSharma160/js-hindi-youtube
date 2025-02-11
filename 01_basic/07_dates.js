0cb9cbf508a6b26bc31eb5c0fd7724d6d801742e
AnupSharma160 <anupsharma9058@gmail.com>
2025-02-11 16:03:51 +0000

    dates.js

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,11 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2025,0,11)
+console.log(myCreateDate.toDateString());
+
